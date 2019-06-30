30aac8b2949a6344bf7f67a1ddaf6fa5b73434a5
zonecc <bkevery@gmail.com>
2019-06-30 21:48:03 +0300

    image issue

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+console.log(__dirname)
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
